1bb4a91de633d6a88bfe7a8827e784d3cdaf8967
Martín Silva Molina <msilvamolina@gmail.com>
2025-03-22 00:43:36 +0100

    feat(auth): add authentication checks to layout components
    Implement authentication checks in the auth and root layout components to redirect users based on their authentication status. This ensures that authenticated users are redirected to the root page and unauthenticated users are redirected to the sign-in page. Additionally, add the `isAuthenticated` function to the auth actions to determine the user's authentication status.

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,7 +1,6 @@
 "use server";
 
 import { db, auth } from "@/firebase/admin";
-import console from "console";
 import { cookies } from "next/headers";
 
 const ONE_WEEK = 60 * 60 * 24 * 7;
@@ -81,4 +80,33 @@ export async function setSessionCookie(idToken: string) {
     path: "/",
     sameSite: "lax",
   });
+}
+
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
+  const sessionCookie = cookieStore.get("session")?.value;
+  if (!sessionCookie) return null;
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie);
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+
+    if (!userRecord.exists) return null;
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+}
+
+export async function isAuthenticated() {
+  const user = await getCurrentUser();
+
+  return !!user;
 }
