ca421c99c13cab1c1ed2803407815e87cd0cb567
Martín Silva Molina <msilvamolina@gmail.com>
2025-03-21 21:50:45 +0100

    feat(AuthForm): enhance form structure and add sign-up/sign-in logic
    Refactor AuthForm to support both sign-up and sign-in flows. Update form schema to conditionally validate fields based on the form type. Add UI improvements including a logo, headings, and navigation links between sign-up and sign-in pages. Handle form submission with error handling and toast notifications.

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -3,61 +3,77 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
+import Image from "next/image";
+import Link from "next/link";
+import { toast } from "sonner";
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-  email: z.string().email(),
-  password: z.string().min(8),
-});
+const authFormSchema = (type: FormType) => {
+  return z.object({
+    name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(3),
+  });
+};
 
-const AuthForm = ({ type }: { type: string }) => {
-  // 1. Define your form.
+const AuthForm = ({ type }: { type: FormType }) => {
+  const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      password: "",
     },
   });
 
-  // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+    try {
+      if (type === "sign-up") {
+        console.log(values);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(`There was an error: ${error}`);
+    }
   }
+  const isSign = type === "sign-in";
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <div className="card-border lg:min-w-[566px">
+      <div className="flex flex-col gap-6 card py-14 px-10">
+        <div className="flex flex-row gap-2 justify-center">
+          <Image src="/logo.svg" alt="logo" height={32} width={38} />
+          <h2 className="text-primary-100">PrepWise</h2>
+        </div>
+        <h3>Practice job interview with AI</h3>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-6 mt-4 form"
+          >
+            {!isSign && <p>Name</p>}
+            <p>Email</p>
+            <p>Password</p>
+
+            <Button className="btn" type="submit">
+              {isSign ? "Sign in" : "Sign up"}
+            </Button>
+          </form>
+        </Form>
+
+        <p className="text-center">
+          {isSign ? "No account yet?" : "Have an account already?"}
+          <Link
+            href={!isSign ? "/sign-in" : "/sign-up"}
+            className="font-bold text-user-primary ml-1"
+          >
+            {!isSign ? "Sign in" : "Sign up"}
+          </Link>
+        </p>
+      </div>
+    </div>
   );
 };
 
